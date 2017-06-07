a900d88328e27c1e526b4611d2a630cb7d88ddca
kbista <kbista@intellimec.com>
2017-06-07 16:30:50 +0545

    Initial commit

---
+++ src/main/java/com/kripesh/salesapp/SalesAppApplication.java
@@ -0,0 +1,12 @@
+package com.kripesh.salesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesAppApplication.class, args);
+	}
+}
